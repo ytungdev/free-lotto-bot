9af3c7c0492e61b1904f34260b306aa86551ad74
ytung <ytung.dev@gmail.com>
2023-05-14 16:48:40 +0100

    added weekly draw if available

--- main.py
+++ main.py
@@ -12,49 +12,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-login_actions = [
-    ("panelButton", "click"),
-    ("Email0", os.getenv('EMAIL')),
-    ("Password0", os.getenv('PASSWORD')),
-    ("submit_login_details", "click")
-]
-
-enroll_actions =[]
-def form_choice():
-    LUCKYNUM = os.getenv('LUCKYNUM', False)
-    print(f"LUCKNUM : {LUCKYNUM}\n")
-    if not LUCKYNUM:
-        choice = []
-    else:
-        choice = [int(n) for n in LUCKYNUM.split(" ")]
-    choices = []
-    for i in range(1,76):
-        i not in choice and choices.append(i)
-    rnd = random.sample(choices, 6-len(choice))
-    choice.extend(rnd)
-    return sorted(choice)
-def choice2action(choice):
-    for n in choice:
-        enroll_actions.append((f"B0ID_{n}", "click"))
-
 def do_login(web):
     print(f"DO : LOGIN")
     web.go_to("https://www.lottery.co.uk/free-lottery")
-    web.do_actions(login_actions)
+    web.do_actions(web.login_actions)
     print()
 
 def do_enrol(web):
-    choice = form_choice()
-    print(f"DO : ENROL - {choice}")
-    choice2action(choice)
+    print(f"DO : ENROL - {web.choice}")
     web.go_to("https://www.lottery.co.uk/free-lottery/play?lottery=daily")
     try:
         enable = web.driver.find_element(By.ID, 'submit_ticket').is_enabled()
-        web.do_actions(enroll_actions)
+        web.do_actions(web.enroll_actions)
         web.do_action('submit_ticket', 'click')
-        print()
     except NoSuchElementException:
         print('UNABLE TO ENROL')
+    print()
+
+def do_weekly(web):
+    print(f"DO : WEEKLY - {web.choice}")
+    web.go_to("https://www.lottery.co.uk/free-lottery/play?lottery=weekly")
+    try:
+        enable = web.driver.find_element(By.ID, 'submit_ticket').is_enabled()
+        web.do_actions(web.enroll_actions)
+        web.do_action('submit_ticket', 'click')
+        enroll_actions =[]
+    except NoSuchElementException:
+        print('UNABLE TO ENROL')
+    print()
 
 class LottoWeb(object):
     def __init__(self) -> None:
@@ -67,6 +52,15 @@ class LottoWeb(object):
             self.driver = webdriver.Chrome(options=options)
         else:
             self.driver = webdriver.Chrome(exe, options=options)
+
+        self.login_actions = [
+            ("panelButton", "click"),
+            ("Email0", os.getenv('EMAIL')),
+            ("Password0", os.getenv('PASSWORD')),
+            ("submit_login_details", "click")
+        ]
+        self.choice = self.gen_choice()
+        self.enroll_actions = [(f"B0ID_{n}", "click") for n in self.choice]
     
     def go_to(self, url):
         self.driver.get(url)
@@ -85,8 +79,24 @@ class LottoWeb(object):
             element.send_keys(action)
         print(f"action : {eid:<20} | {action}")
 
+    def gen_choice(self):
+        LUCKYNUM = os.getenv('LUCKYNUM', False)
+        print(f"LUCKNUM : {LUCKYNUM}\n")
+        if not LUCKYNUM:
+            choice = []
+        else:
+            choice = [int(n) for n in LUCKYNUM.split(" ")]
+        choices = []
+        for i in range(1,76):
+            i not in choice and choices.append(i)
+        rnd = random.sample(choices, 6-len(choice))
+        choice.extend(rnd)
+        return sorted(choice)
+
 if __name__ == '__main__':
     web = LottoWeb()
     do_login(web)
+    ## wait 0.5 sec for website redirection
     time.sleep(0.5)
     do_enrol(web)
+    do_weekly(web)
